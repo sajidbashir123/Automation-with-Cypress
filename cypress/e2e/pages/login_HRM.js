49ba954008277b4beebd081f1b556c273debf294
sajidbashir123 <sajidbashir278@gmail.com>
2022-11-28 18:08:07 +0500

    all login test

--- cypress/e2e/pages/login_HRM.js
+++ cypress/e2e/pages/login_HRM.js
@@ -1,11 +1,11 @@
 export class Login{
     username(name)
     {
-        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type("Admin");
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(name);
     }
     password(password)
     {
-        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type("admin123");
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password);
     }
     clicklogin()
     { 
--- cypress/e2e/pom_hrm.cy.js
+++ cypress/e2e/pom_hrm.cy.js
@@ -1,8 +1,36 @@
 import { Login } from "./pages/login_HRM";
 const login = new Login()
 
-it("Page object model", ()=>{
-    cy.visit ("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+describe('Login Test', () => {
+
+
+    it("Valid-login", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        login.username("Admin");
+        login.password("admin123");
+        login.clicklogin();
+    })
+
+    it.skip ("Invalid-login", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"); // it.skip-->this comaand use to skip that specifit it block 
+        login.username("admin23");
+        login.password("1234");
+        login.clicklogin();
+    })
+
+    /*
+    it.only ("Empty-login", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"); // it.only-->comaand use to execute that specifit it block only
+        login.username("Admin");
+        login.password("sad");
+        login.clicklogin();
+    })
+        */
+})
+
+
+it ("Out side the Describe block", () => {
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"); //this it block contain the outside of the describe block
     login.username("Admin");
     login.password("admin123");
     login.clicklogin();
